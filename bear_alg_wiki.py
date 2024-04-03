e8e479913df1850479ce12b164d92302b55b8510
iwitczak <igor.witczak@outlook.com>
2024-04-03 09:50:06 +0200

    juz blisko

--- bear_alg_wiki.py
+++ bear_alg_wiki.py
@@ -3,12 +3,12 @@ import numpy as np
 from numpy.random import default_rng
 import open3d as o3d
 import laspy
-rng = default_rng()
 
+rng = default_rng()
 
 # Czesc z wikipedii
 class RANSAC:
-    def __init__(self, n=10, k=100, t=0.05, d=10, model=None, loss=None, metric=None):
+    def __init__(self, n=50000, k=1000, t=0.01, d=100, model=None, loss=None, metric=None):
         self.n = n              # `n`: Minimum number of data points to estimate parameters
         self.k = k              # `k`: Maximum iterations allowed
         self.t = t              # `t`: Threshold value to determine if points are fit well
@@ -64,14 +64,14 @@ class LinearRegressor:
 
     def fit(self, X: np.ndarray, y: np.ndarray):
         r, _ = X.shape
-        X = np.hstack([np.ones((r, 1)), np.cosh(X)]) # rysunek z zeszytu
+        X = np.hstack([np.ones((r, 1)), -np.cosh(X)]) # rysunek z zeszytu
         # print(X)
         self.params = np.linalg.inv(X.T @ X) @ X.T @ y
         return self
 
     def predict(self, X: np.ndarray):
         r, _ = X.shape
-        X = np.hstack([np.ones((r, 1)), np.cosh(X)]) # rysunek z zeszytu
+        X = np.hstack([np.ones((r, 1)), -np.cosh(X)]) # rysunek z zeszytu
         return X @ self.params
 
 
@@ -103,6 +103,7 @@ def o3d_load_point_cloud(path):
     pt_cloud = o3d.geometry.PointCloud()
     pt_cloud.points = o3d.utility.Vector3dVector(las_xyz)
     pt_cloud.colors = o3d.utility.Vector3dVector(las_colors) 
+    print(pt_cloud)
     return pt_cloud
     
 
@@ -127,25 +128,44 @@ def o3d_choose_points(pt_cloud):
 
 
 if __name__ == "__main__":
-    visualize_point_cloud(o3d_load_point_cloud(r"C:\Users\qattr\Desktop\STUD\SEM 6\FTP2\otwock_freski18_n.las"))
-    # regressor = RANSAC(model=LinearRegressor(), loss=square_error_loss, metric=mean_square_error)
-
-    # X = np.array([-0.848,-0.800,-0.704,-0.632,-0.488,-0.472,-0.368,-0.336,-0.280,-0.200,-0.00800,-0.0840,0.0240,0.100,0.124,0.148,0.232,0.236,0.324,0.356,0.368,0.440,0.512,0.548,0.660,0.640,0.712,0.752,0.776,0.880,0.920,0.944,-0.108,-0.168,-0.720,-0.784,-0.224,-0.604,-0.740,-0.0440,0.388,-0.0200,0.752,0.416,-0.0800,-0.348,0.988,0.776,0.680,0.880,-0.816,-0.424,-0.932,0.272,-0.556,-0.568,-0.600,-0.716,-0.796,-0.880,-0.972,-0.916,0.816,0.892,0.956,0.980,0.988,0.992,0.00400]).reshape(-1,1)
-    ## ogyginalne - z wiki wartosci liniowe
-    ## y = np.array([-0.917,-0.833,-0.801,-0.665,-0.605,-0.545,-0.509,-0.433,-0.397,-0.281,-0.205,-0.169,-0.0531,-0.0651,0.0349,0.0829,0.0589,0.175,0.179,0.191,0.259,0.287,0.359,0.395,0.483,0.539,0.543,0.603,0.667,0.679,0.751,0.803,-0.265,-0.341,0.111,-0.113,0.547,0.791,0.551,0.347,0.975,0.943,-0.249,-0.769,-0.625,-0.861,-0.749,-0.945,-0.493,0.163,-0.469,0.0669,0.891,0.623,-0.609,-0.677,-0.721,-0.745,-0.885,-0.897,-0.969,-0.949,0.707,0.783,0.859,0.979,0.811,0.891,-0.137]).reshape(-1,1)
     
-    # # wygenerowane x**2
+    pt_cld = o3d_load_point_cloud(r"C:\Users\qattr\Desktop\STUD\SEM 6\FTP2\luk3.las")
+    np_arr_pts = np.asarray(pt_cld.points)
+    new_arr = np_arr_pts[:,:2]
+    print(new_arr)
+    new_X = np_arr_pts[:,1].reshape(-1,1)
+    new_Y = np_arr_pts[:,2].reshape(-1,1)
+    
+    new_X = new_X - new_X.mean()
+    new_Y = new_Y - new_Y.mean()
+    print(new_Y)
+    
+    
+    
+    
+    
+    
+    
+    # # visualize_point_cloud(o3d_load_point_cloud(r"C:\Users\qattr\Desktop\STUD\SEM 6\FTP2\otwock_freski18_n.las"))
+    regressor = RANSAC(model=LinearRegressor(),loss=square_error_loss, metric=mean_square_error)
+
+    X = np.array([-0.848,-0.800,-0.704,-0.632,-0.488,-0.472,-0.368,-0.336,-0.280,-0.200,-0.00800,-0.0840,0.0240,0.100,0.124,0.148,0.232,0.236,0.324,0.356,0.368,0.440,0.512,0.548,0.660,0.640,0.712,0.752,0.776,0.880,0.920,0.944,-0.108,-0.168,-0.720,-0.784,-0.224,-0.604,-0.740,-0.0440,0.388,-0.0200,0.752,0.416,-0.0800,-0.348,0.988,0.776,0.680,0.880,-0.816,-0.424,-0.932,0.272,-0.556,-0.568,-0.600,-0.716,-0.796,-0.880,-0.972,-0.916,0.816,0.892,0.956,0.980,0.988,0.992,0.00400]).reshape(-1,1)
+    # ## ogyginalne - z wiki wartosci liniowe
+    # print(X)
+    Y = np.array([-0.917,-0.833,-0.801,-0.665,-0.605,-0.545,-0.509,-0.433,-0.397,-0.281,-0.205,-0.169,-0.0531,-0.0651,0.0349,0.0829,0.0589,0.175,0.179,0.191,0.259,0.287,0.359,0.395,0.483,0.539,0.543,0.603,0.667,0.679,0.751,0.803,-0.265,-0.341,0.111,-0.113,0.547,0.791,0.551,0.347,0.975,0.943,-0.249,-0.769,-0.625,-0.861,-0.749,-0.945,-0.493,0.163,-0.469,0.0669,0.891,0.623,-0.609,-0.677,-0.721,-0.745,-0.885,-0.897,-0.969,-0.949,0.707,0.783,0.859,0.979,0.811,0.891,-0.137]).reshape(-1,1)
+    
+    # # # wygenerowane x**2
     # y_values = -(X)**2
     # y = np.array(y_values).reshape(-1,1)
 
-    # regressor.fit(X, y)
+    regressor.fit(new_X, new_Y)
 
-    # import matplotlib.pyplot as plt
-    # ax = plt.figure().add_subplot(projection='3d')
+    import matplotlib.pyplot as plt
+    ax = plt.figure().add_subplot(projection='3d')
 
-    # # Scatter punktow z list X i y
-    # ax.scatter(X, y, zs=0, zdir='z')
+    # Scatter punktow z list X i y
+    ax.scatter(new_X, new_Y, zs=0, zdir='z')
 
-    # line = np.linspace(-1, 1, num=100).reshape(-1, 1)
-    # ax.plot(line, regressor.predict(line),zs=0, zdir='z', c="peru")
-    # plt.show()
+    line = np.linspace(new_X.min(), new_X.max(), num=100).reshape(-1, 1)
+    ax.plot(line, regressor.predict(line),zs=0, zdir='z', c="peru")
+    plt.show()
