748019cf7094c709178bbaf2ff20752a0a925033
Tymon <tyren234@gmail.com>
2024-04-04 12:17:31 +0200

    Implemented pint_picker.py

--- app/functions/point_picker.py
+++ app/functions/point_picker.py
@@ -1,5 +1,43 @@
+import tkinter.messagebox
+
 import open3d as o3d
 
+from app.common.file_dialogs import get_cloud_filename, get_savefile_path
 
-def pick_point(cloud: o3d.geometry.PointCloud) -> None:
-    pass
+
+def manual_point_picking(point_cloud: o3d.geometry.PointCloud):
+    message = "Wybierz punkt.\n1. Naciśnij shift + lpm, żeby wybrać punkt.\n2.Naciśnij shift + ppm, żeby cofnąć wybranie punktu.\n4. Naciśnij 'Q', żeby zakończyć i zapisać."
+    tkinter.messagebox.showinfo(title='Instrukcja do wybierania punktów', message=message)
+
+    vis = o3d.visualization.VisualizerWithEditing()
+    vis.create_window(window_name='Manual point picking')
+    vis.add_geometry(point_cloud)
+    vis.run()  # user picks points
+    vis.destroy_window()
+    return vis.get_picked_points()
+
+
+def pick_point() -> None:
+    tkinter.messagebox.showinfo(title='Informacja', message="Wybierz chmurę punktów łuku.")
+    filename: str = get_cloud_filename()
+    tkinter.messagebox.showinfo(title='Informacja', message="Gdzie zapisać wybrany punkt.")
+    filetypes = ['.pcd', '.ply']
+    save_filename: str = get_savefile_path()
+    cloud: o3d.geometry.PointCloud = o3d.io.read_point_cloud(filename)
+
+    points_indexes = manual_point_picking(cloud)
+    if len(points_indexes) != 1:
+        print(f"Aby zapisać należy wybrać dokładnie 1 punkt, a nie {len(points_indexes)}. Przerywanie.")
+        return
+
+    # save_filename = get_savefile_path()
+    if save_filename == "" or not any([save_filename.endswith(filetype) for filetype in filetypes]):
+        print(f"Aby zapisać należy wybrać plik do zapisu {filetypes}. Przerywanie.")
+        return
+
+    o3d.io.write_point_cloud(save_filename, cloud.select_by_index(points_indexes), write_ascii=False, compressed=False,
+                             print_progress=False)
+
+
+if __name__ == "__main__":
+    pick_point()
