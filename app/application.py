6f816a14b7ddb5ee1815c88d4d7671faa8d8e126
Tymon <tyren234@gmail.com>
2024-04-10 09:51:03 +0200

    Changed menu message to reflect the current state of arch and middle point beeing loaded or not

--- app/application.py
+++ app/application.py
@@ -1,19 +1,29 @@
+import open3d as o3d
+
+from app.common.file_dialogs import get_cloud_filename
 from functions.arch_cutter import cut_arch
 from functions.arch_fitter import fit_arch
 from functions.point_picker import pick_point
 
-menu_message = ["Co chcesz zrobić?",
-                "0. Wyjść.",
-                "1. Wyciąć łuk z chmury punktów.",
-                "2. Wczytać łuk z pliku.",
-                "3. Wybrać punkt śrokowy łuku z chmury punktów.",
-                "4. Wczytać punkt środkowy z pliku.",
-                "5. Wpasować płaszczyznę w łuk (najpierw wczytaj łuk i punkt środkowy)", ]
-
-exit_commands = ["e", "exit", "leave", "q", "quit", "end", "finish", "3"]
-
 if __name__ == "__main__":
+    exit_commands = ["e", "exit", "leave", "q", "quit", "end", "finish", "3"]
+
+    arch: o3d.geometry.PointCloud = None
+    middle_point: o3d.geometry.PointCloud = None
+
+    menu_message = ["Co chcesz zrobić?",
+                    "0. Wyjść.",
+                    "1. Wyciąć łuk z chmury punktów.",
+                    "2. Wczytać łuk z pliku.",
+                    "3. Wybrać punkt śrokowy łuku z chmury punktów.",
+                    "4. Wczytać punkt środkowy z pliku.",
+                    "5. Wpasować płaszczyznę w łuk. \n Najpierw załaduj łuk [" + (
+                        "❌" if arch is None else "✔") + "] i punkt środkowy [" + (
+                        "❌" if middle_point is None else "✔") + "]"]
+
     while True:
+        menu_message[6] = "5. Wpasować płaszczyznę w łuk. \n Najpierw załaduj łuk [" + (
+            "❌" if arch is None else "✔") + "] i punkt środkowy [" + ("❌" if middle_point is None else "✔") + "]"
         print("", *menu_message, sep="\n")
 
         try:
