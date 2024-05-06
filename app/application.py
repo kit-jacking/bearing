9f2f02abb12805d4962042d4f32eb07b65997ce0
Tymon <tyren234@gmail.com>
2024-05-06 11:16:19 +0200

    Renamed middle_point to key_points

--- app/application.py
+++ app/application.py
@@ -9,21 +9,21 @@ if __name__ == "__main__":
     exit_commands = ["e", "exit", "leave", "q", "quit", "end", "finish", "3"]
 
     arch: o3d.geometry.PointCloud = None
-    middle_point: o3d.geometry.PointCloud = None
+    key_points: o3d.geometry.PointCloud = None
 
     menu_message = ["Co chcesz zrobić?",
                     "0. Wyjść.",
                     "1. Wyciąć łuk z chmury punktów.",
                     "2. Wczytać łuk z pliku.",
-                    "3. Wybrać punkt śrokowy łuku z chmury punktów.",
-                    "4. Wczytać punkt środkowy z pliku.",
+                    "3. Wybrać 4 punkty kluczowe łuku z chmury punktów.",
+                    "4. Wczytać 4 punkty kluczowe łuku z pliku.",
                     "5. Wpasować płaszczyznę w łuk. \n Najpierw załaduj łuk [" + (
-                        "❌" if arch is None else "✔") + "] i punkt środkowy [" + (
-                        "❌" if middle_point is None else "✔") + "]"]
-    
+                        "❌" if arch is None else "✔") + "] i 4 punkty kluczowe [" + (
+                        "❌" if key_points is None else "✔") + "]"]
+
     while True:
         menu_message[6] = "5. Wpasować płaszczyznę w łuk. \n Najpierw załaduj łuk [" + (
-            "❌" if arch is None else "✔") + "] i punkt środkowy [" + ("❌" if middle_point is None else "✔") + "]"
+            "❌" if arch is None else "✔") + "] i 4 punkty kluczowe łuku [" + ("❌" if key_points is None else "✔") + "]"
         print("", *menu_message, sep="\n")
 
         try:
@@ -39,15 +39,15 @@ if __name__ == "__main__":
                 pick_point()
             elif typed.lower() == "4":
                 filename: str = get_cloud_filename()
-                middle_point: o3d.geometry.PointCloud = o3d.io.read_point_cloud(filename)
-                if len(middle_point.points) != 1:
-                    middle_point = None
-                    print("Plik z punktem środkowym powinien zawierać dokładnie jeden punkt. Sprawdź czy wybrano odpowiedni plik.")
-                    raise RuntimeError("Middle point should have exactly one point.")
+                key_points: o3d.geometry.PointCloud = o3d.io.read_point_cloud(filename)
+                if len(key_points.points) != 4:
+                    key_points = None
+                    print("Plik z 4 punktami kluczowymi łuku powinien zawierać dokładnie cztery punkty. Sprawdź czy wybrano odpowiedni plik.")
+                    raise RuntimeError("Key points should have exactly four points.")
             elif typed.lower() == "5":
-                if middle_point is None or arch is None:
-                    print("Punkt środkowy i łuk muszą zostać najpierw wczytane.")
-                    raise RuntimeError("Middle point and arch should not be None.")
+                if key_points is None or arch is None:
+                    print("4 punkty kluczowe łuku i łuk muszą zostać najpierw wczytane.")
+                    raise RuntimeError("Key points and arch should not be 'None'.")
                 fit_arch()
             else:
                 print("Nieznana komenda")
