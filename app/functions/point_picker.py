93e679dc82e30a503710effdb2168beeb5dbead4
Tymon <tyren234@gmail.com>
2024-05-06 11:29:18 +0200

    Refactored point picker instructions and made it accept 4 points instead of 1

--- app/functions/point_picker.py
+++ app/functions/point_picker.py
@@ -6,8 +6,10 @@ from app.common.file_dialogs import get_cloud_filename, get_savefile_path
 
 
 def manual_point_picking(point_cloud: o3d.geometry.PointCloud):
-    message = "Wybierz punkt.\n1. Naciśnij shift + lpm, żeby wybrać punkt.\n2.Naciśnij shift + ppm, żeby cofnąć wybranie punktu.\n4. Naciśnij 'Q', żeby zakończyć i zapisać."
+    message = "Wybierz punkt.\n1. Naciśnij shift + lpm, żeby wybrać punkt.\n2.Naciśnij shift + ppm, żeby cofnąć wybranie punktu.\n3. Naciśnij 'Q', żeby zakończyć i zapisać.\n"
     tkinter.messagebox.showinfo(title='Instrukcja do wybierania punktów', message=message)
+    message = ("!UWAGA!\n Punkty należy wybrać w ściśle określonej klejności! W przeciwnym wypadku łuk zostanie źle wpasowany.\n\n1. Punkt na dole łuku, przy końcu z jednej strony. Tworzy płaszczyznę z punktami 2 i 3.\n2. Punkt z drugiej strony łuku, również na dole, w tej samej płaszczyźnie co putnk 1 i 3.\n 3. Punkt środkowy u szczytu łuku, w tej samej płaszczyźnie co punkty 1 i 2.\n 4. Punkt u szczytu łuku, po drugiej stronie łuku niż punkt 3. Punkt 4 odpowiada za głębię.\n Pamiętaj wybrać punkty dokładnie w tej kolejności i o tym, że możesz cofać używając shift + ppm.\n\n Powodzenia!")
+    tkinter.messagebox.showwarning(title='Instrukcja do wybierania punktów', message=message)
 
     vis = o3d.visualization.VisualizerWithEditing()
     vis.create_window(window_name='Manual point picking')
@@ -26,8 +28,8 @@ def pick_point() -> None:
     cloud: o3d.geometry.PointCloud = o3d.io.read_point_cloud(filename)
 
     points_indexes = manual_point_picking(cloud)
-    if len(points_indexes) != 1:
-        print(f"Aby zapisać należy wybrać dokładnie 1 punkt, a nie {len(points_indexes)}. Przerywanie.")
+    if len(points_indexes) != 4:
+        print(f"Aby zapisać należy wybrać dokładnie 4 punkty, a nie {len(points_indexes)}. Przerywanie.")
         return
 
     # save_filename = get_savefile_path()
