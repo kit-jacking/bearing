71ef93e885ecd67411a86e3bd05a54c65ae70a6b
Tymon <tyren234@gmail.com>
2024-05-27 09:34:26 +0200

    Application merged with algrytm_wiki_RANSAC.py

--- app/application.py
+++ app/application.py
@@ -1,5 +1,6 @@
 import open3d as o3d
 
+from app.functions.algorytm_wiki_RANSAC import find_fit
 from app.functions.file_dialogs import get_cloud_filename
 from functions.arch_cutter import cut_arch
 from functions.arch_fitter import fit_arch
@@ -10,7 +11,8 @@ if __name__ == "__main__":
 
     arch: o3d.geometry.PointCloud = None
     key_points: o3d.geometry.PointCloud = None
-
+    arch_filename: str = None
+    keypoints_filename: str = None
     menu_message = ["Co chcesz zrobić?",
                     "0. Wyjść.",
                     "1. Wyciąć łuk z chmury punktów.",
@@ -18,12 +20,12 @@ if __name__ == "__main__":
                     "3. Wybrać 4 punkty kluczowe łuku z chmury punktów.",
                     "4. Wczytać 4 punkty kluczowe łuku z pliku.",
                     "5. Wpasować płaszczyznę w łuk. \n Najpierw załaduj łuk [" + (
-                        "❌" if arch is None else "✔") + "] i 4 punkty kluczowe [" + (
-                        "❌" if key_points is None else "✔") + "]"]
+                        "❌" if arch_filename is None else "✔") + "] i 4 punkty kluczowe [" + (
+                        "❌" if keypoints_filename is None else "✔") + "]"]
 
     while True:
         menu_message[6] = "5. Wpasować płaszczyznę w łuk. \n Najpierw załaduj łuk [" + (
-            "❌" if arch is None else "✔") + "] i 4 punkty kluczowe łuku [" + ("❌" if key_points is None else "✔") + "]"
+            "❌" if arch_filename is None else "✔") + "] i 4 punkty kluczowe łuku [" + ("❌" if keypoints_filename is None else "✔") + "]"
         print("", *menu_message, sep="\n")
 
         try:
@@ -34,23 +36,23 @@ if __name__ == "__main__":
             elif typed.lower() == "1":
                 cut_arch()
             elif typed.lower() == "2":
-                filename: str = get_cloud_filename()
-                arch: o3d.geometry.PointCloud = o3d.io.read_point_cloud(filename)
+                arch_filename: str = get_cloud_filename()
+                arch: o3d.geometry.PointCloud = o3d.io.read_point_cloud(arch_filename)
             elif typed.lower() == "3":
                 pick_point()
             elif typed.lower() == "4":
-                filename: str = get_cloud_filename()
-                key_points: o3d.geometry.PointCloud = o3d.io.read_point_cloud(filename)
+                keypoints_filename: str = get_cloud_filename()
+                key_points: o3d.geometry.PointCloud = o3d.io.read_point_cloud(keypoints_filename)
                 if len(key_points.points) != 4:
                     key_points = None
                     print(
                         "Plik z 4 punktami kluczowymi łuku powinien zawierać dokładnie cztery punkty. Sprawdź czy wybrano odpowiedni plik.")
                     raise RuntimeError("Key points should have exactly four points.")
             elif typed.lower() == "5":
-                if key_points is None or arch is None:
+                if keypoints_filename is None or arch_filename is None:
                     print("4 punkty kluczowe łuku i łuk muszą zostać najpierw wczytane.")
                     raise RuntimeError("Key points and arch should not be 'None'.")
-                fit_arch(key_points, arch)
+                find_fit(arch_filename, keypoints_filename)
             else:
                 print("Nieznana komenda")
         except:
--- app/functions/algorytm_wiki_RANSAC.py
+++ app/functions/algorytm_wiki_RANSAC.py
@@ -3,21 +3,19 @@ import open3d as o3d
 import matplotlib.pyplot as plt
 from copy import copy
 from numpy.random import default_rng
-import laspy
-
-print(laspy.__version__)
 
 rng = default_rng()
 
+
 class RANSAC:
     def __init__(self, n=10, k=2000, t=0.01, d=50, model=None, loss=None, metric=None):
-        self.n = n              # `n`: Minimum number of data points to estimate parameters
-        self.k = k              # `k`: Maximum iterations allowed
-        self.t = t              # `t`: Threshold value to determine if points are fit well
-        self.d = d              # `d`: Number of close data points required to assert model fits well
-        self.model = model      # `model`: class implementing `fit` and `predict`
-        self.loss = loss        # `loss`: function of `y_true` and `y_pred` that returns a vector
-        self.metric = metric    # `metric`: function of `y_true` and `y_pred` and returns a float
+        self.n = n  # `n`: Minimum number of data points to estimate parameters
+        self.k = k  # `k`: Maximum iterations allowed
+        self.t = t  # `t`: Threshold value to determine if points are fit well
+        self.d = d  # `d`: Number of close data points required to assert model fits well
+        self.model = model  # `model`: class implementing `fit` and `predict`
+        self.loss = loss  # `loss`: function of `y_true` and `y_pred` that returns a vector
+        self.metric = metric  # `metric`: function of `y_true` and `y_pred` and returns a float
         self.best_fit = None
         self.best_error = np.inf
 
@@ -29,11 +27,11 @@ class RANSAC:
             maybe_model = copy(self.model).fit(X[maybe_inliers], y[maybe_inliers])
 
             thresholded = (
-                self.loss(y[ids][self.n :], maybe_model.predict(X[ids][self.n :]))
-                < self.t
+                    self.loss(y[ids][self.n:], maybe_model.predict(X[ids][self.n:]))
+                    < self.t
             )
 
-            inlier_ids = ids[self.n :][np.flatnonzero(thresholded).flatten()]
+            inlier_ids = ids[self.n:][np.flatnonzero(thresholded).flatten()]
 
             if inlier_ids.size > self.d:
                 inlier_points = np.hstack([maybe_inliers, inlier_ids])
@@ -52,11 +50,14 @@ class RANSAC:
     def predict(self, X):
         return self.best_fit.predict(X)
 
+
 def square_error_loss(y_true, y_pred):
     return (y_true - y_pred) ** 2
 
+
 def mean_square_error(y_true, y_pred):
     return np.sum(square_error_loss(y_true, y_pred)) / y_true.shape[0]
+
 
 class HyperbolicRegressor:
     def __init__(self):
@@ -72,27 +73,29 @@ class HyperbolicRegressor:
         r, _ = X.shape
         X = np.hstack([np.ones((r, 1)), np.cosh(X)])
         return X @ self.params
-    
+
+
 class QuadraticRegressor:
     def __init__(self):
         self.params = None
 
     def fit(self, X: np.ndarray, y: np.ndarray):
         r, _ = X.shape
-        X = np.hstack([np.ones((r, 1)), X, X**2])
+        X = np.hstack([np.ones((r, 1)), X, X ** 2])
         self.params = np.linalg.inv(X.T @ X) @ X.T @ y
         return self
 
     def predict(self, X: np.ndarray):
         r, _ = X.shape
-        X = np.hstack([np.ones((r, 1)), X, X**2])
+        X = np.hstack([np.ones((r, 1)), X, X ** 2])
         return X @ self.params
-    
-def las_to_o3d(file): 
-    las_pcd = laspy.file.File(file, mode = 'r') 
-    x = las_pcd.x 
+
+
+def las_to_o3d(file):
+    las_pcd = laspy.file.File(file, mode='r')
+    x = las_pcd.x
     y = las_pcd.y
-    z = las_pcd.z 
+    z = las_pcd.z
 
     # Normalizacja szarosci
     r = las_pcd.intensity / max(las_pcd.intensity)
@@ -109,7 +112,8 @@ def las_to_o3d(file):
 
     return pcd_o3d
 
-def equation_plane(x1, y1, z1, x2, y2, z2, x3, y3, z3): 
+
+def equation_plane(x1, y1, z1, x2, y2, z2, x3, y3, z3):
     a1 = x2 - x1
     b1 = y2 - y1
     c1 = z2 - z1
@@ -120,9 +124,10 @@ def equation_plane(x1, y1, z1, x2, y2, z2, x3, y3, z3):
     b = a2 * c1 - a1 * c2
     c = a1 * b2 - b1 * a2
     d = (- a * x1 - b * y1 - c * z1)
-    print ("equation of plane is ", a, "x +", b, "y +", c, "z +", d, "= 0.")
+    print("equation of plane is ", a, "x +", b, "y +", c, "z +", d, "= 0.")
 
     return a, b, c, d
+
 
 def point_on_plane(plane_equation, point):
     a, b, c, d = plane_equation
@@ -133,6 +138,7 @@ def point_on_plane(plane_equation, point):
     else:
         return False
 
+
 def filter_points_by_limits(points, lower_limit, upper_limit):
     filtered_points = []
     for point in points:
@@ -140,10 +146,13 @@ def filter_points_by_limits(points, lower_limit, upper_limit):
             filtered_points.append(point)
     return np.array(filtered_points)
 
+
 def map_to_2d(points_3d):
     return points_3d[:, :2]
 
-def visualize_points(left_point, right_point, center_point, depth_point, points_on_plane, line_points, depth_line_points):
+
+def visualize_points(left_point, right_point, center_point, depth_point, points_on_plane, line_points,
+                     depth_line_points, point_cloud: o3d.geometry.PointCloud):
     # Utworzenie obiektu LineSet
     line_set = o3d.geometry.LineSet()
 
@@ -152,7 +161,7 @@ def visualize_points(left_point, right_point, center_point, depth_point, points_
     line_set.points = o3d.utility.Vector3dVector(line_points_all)
 
     # Definiowanie linii łączących kolejne punkty
-    lines = [[i, i+1] for i in range(len(line_points) - 1)]
+    lines = [[i, i + 1] for i in range(len(line_points) - 1)]
     lines += [[i, i + len(line_points)] for i in range(len(line_points))]
     line_set.lines = o3d.utility.Vector2iVector(lines)
 
@@ -180,13 +189,13 @@ def visualize_points(left_point, right_point, center_point, depth_point, points_
 
     # Wygładzanie siatki (opcjonalnie)
     mesh.compute_vertex_normals()
-    
+
     # Tworzenie obiektu PointCloud dla pierwotnych punktów
     original_points = o3d.geometry.PointCloud()
     original_points.points = o3d.utility.Vector3dVector(points_on_plane)
 
     # Ustawienie koloru dla pierwotnych punktów (opcjonalnie)
-    original_colors = np.array([[0.5, 0.6, 0.6 ] for _ in range(len(points_on_plane))])  # szare punkty
+    original_colors = np.array([[0.5, 0.6, 0.6] for _ in range(len(points_on_plane))])  # szare punkty
     original_points.colors = o3d.utility.Vector3dVector(original_colors)
 
     # Zdefiniowanie rozmiaru punktów left, right i center
@@ -233,23 +242,22 @@ def visualize_points(left_point, right_point, center_point, depth_point, points_
     opt = vis.get_render_option()
     opt.mesh_show_back_face = True
 
+    # chmura punktów
+    vis.add_geometry(point_cloud)
+
     vis.run()
     vis.destroy_window()
 
+
 def find_fit(arc_filepath, points_filepath):
-
-    if arc_filepath.endswith(".las"):
-        arc_o3d = las_to_o3d(arc_filepath)
-    elif arc_filepath.endswith(".ply") or arc_filepath.endswith(".pcd"):
+    if arc_filepath.endswith(".ply") or arc_filepath.endswith(".pcd"):
         arc_o3d: o3d.geometry.PointCloud = o3d.io.read_point_cloud(arc_filepath)
-    arc_pts = np.asarray(arc_o3d.points)  
+    arc_pts = np.asarray(arc_o3d.points)
 
-    if points_filepath.endswith(".las"):
-        pts = las_to_o3d(points_filepath)
-    elif points_filepath.endswith(".ply") or points_filepath.endswith(".pcd"):
+    if points_filepath.endswith(".ply") or points_filepath.endswith(".pcd"):
         pts: o3d.geometry.PointCloud = o3d.io.read_point_cloud(points_filepath)
 
-    arc_pts = np.asarray(arc_o3d.points) 
+    arc_pts = np.asarray(arc_o3d.points)
 
     left_point = np.asarray(pts.points)[0]
     right_point = np.asarray(pts.points)[1]
@@ -263,7 +271,8 @@ def find_fit(arc_filepath, points_filepath):
     lower_limit = np.min([left_point[2], right_point[2]])
 
     # Równanie płaszczyzny
-    plane_equation = equation_plane(center_point[0], center_point[1], center_point[2], left_point[0], left_point[1], left_point[2], right_point[0], right_point[1], right_point[2])
+    plane_equation = equation_plane(center_point[0], center_point[1], center_point[2], left_point[0], left_point[1],
+                                    left_point[2], right_point[0], right_point[1], right_point[2])
 
     # Znalezienie punktów należących do płaszczyzny
     points_on_plane = []
@@ -277,13 +286,13 @@ def find_fit(arc_filepath, points_filepath):
     # points_2d = map_to_2d(points_on_plane)
 
     # Przesunięcie wierzchołka wykresu 
-    X_ = points_on_plane[:, 1].reshape(-1,1)
+    X_ = points_on_plane[:, 1].reshape(-1, 1)
     X = X_ - center_point[1]
-    y_ = points_on_plane[:, 2].reshape(-1,1)
+    y_ = points_on_plane[:, 2].reshape(-1, 1)
     y = y_ - center_point[2]
 
     # Dopasowanie punktow 
-    regressor = RANSAC(model=QuadraticRegressor(), loss=square_error_loss, metric=mean_square_error)
+    regressor = RANSAC(model=HyperbolicRegressor(), loss=square_error_loss, metric=mean_square_error)
     regressor.fit(X, y)
 
     # Wygenerowanie punktów na dopasowanej linii regresji
@@ -302,9 +311,10 @@ def find_fit(arc_filepath, points_filepath):
     depth_line_points = np.copy(line_points)
     depth_line_points[:, 0] -= depth_dist
 
-    visualize_points(left_point, right_point, center_point, depth_point, points_on_plane, line_points, depth_line_points)
+    visualize_points(left_point, right_point, center_point, depth_point, points_on_plane, line_points,
+                     depth_line_points, arc_o3d)
+
 
 if __name__ == "__main__":
-    
-    find_fit(r"C:\Users\julia\Documents\GEOINFORMATYKA\sem6\APFiWM\projekt_sem\luki\luk1.las", 
-                r"C:\Users\julia\Documents\GEOINFORMATYKA\sem6\APFiWM\projekt_sem\luki\luk1_4pts_julka.las")
+    find_fit(r"C:\Users\julia\Documents\GEOINFORMATYKA\sem6\APFiWM\projekt_sem\luki\luk1.las",
+             r"C:\Users\julia\Documents\GEOINFORMATYKA\sem6\APFiWM\projekt_sem\luki\luk1_4pts_julka.las")
